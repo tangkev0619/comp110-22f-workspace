10e821e1b57ca94f32e9e3b9c90d6ad8549e1254
Kevin Tang <tangkev@ad.unc.edu>
2022-08-26 17:36:47 -0400

    Progress on Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,34 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+__author__ = "730578568"
+
+user_name: str = input("Enter a 5-character word:")
+if len(user_name) < 5 or len(user_name) > 5:
+    print("Error: Word must contain 5 characters") 
+    exit()
+user_char: str = input("Enter a single character:")
+if len(user_char) < 1 or len (user_char) > 1:
+    print("Error: Character must be a single character.")
+    exit()
+print("Searching for " + user_char + " in " + user_name)
+counter: int = 0
+if user_char == user_name[0]:
+    print(user_char + " found in index 0")
+    counter+=1
+if user_char == user_name[1]:
+    print(user_char + " found in index 1")
+    counter+=1
+if user_char == user_name[2]:
+    print(user_char + " found in index 2")
+    counter+=1
+if user_char == user_name[3]:
+    print(user_char + " found in index 3")
+    counter+=1
+if user_char == user_name[4]:
+    print(user_char + " found in index 4")
+    counter+=1
+if counter==0:
+    print("No instances of " + user_char + " found in " + user_name)
+elif counter==1:
+    print("1 instance of " + user_char + " found in " + user_name)
+else: 
+    print("2 instances of " + user_name + " found in " + user_name)
