27a8fbecab5f187ee42bddbd1b38c69c2cd116a6
Kevin Tang <tangkev@ad.unc.edu>
2022-09-16 13:47:40 -0400

    Finished EX04 - Utils

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -4,6 +4,8 @@ __author__ = "730578568"
 
 def all(list1: list[int], random: int) -> bool:
     """This function is used to check whether or not if the given number matches with the list for all."""
+    if len(list1) == 0:
+        return False
     i: int = 0
     while i < len(list1):
         if list1[i] != random:
@@ -17,7 +19,7 @@ def max(input: list[int]) -> int:
     if len(input) == 0:
         raise ValueError("max() arg is an empty list")
     i: int = 0
-    valone: int= input[0]
+    valone: int = input[0]
     while i < len(input):
         if valone < input[i]:
             valone = input[i] 
@@ -27,9 +29,11 @@ def max(input: list[int]) -> int:
 
 def is_equal(input1: list[int], input2: list[int]) -> bool:
     """This function is used to check if both list are equal to each other if not it wil return False, if correct it will return True."""
+    if len(input1) != len(input2):
+        return False
     i: int = 0
     while i < len(input1):
-        if input1[i] == input2[i]:
-            return True
+        if input1[i] != input2[i]:
+            return False
         i += 1
-    return False
+    return True
