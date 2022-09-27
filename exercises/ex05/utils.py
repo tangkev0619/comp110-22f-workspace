a3c8c59c5c8c86c8708e8fffafcf6d416d4d9299
Kevin Tang <tangkev@ad.unc.edu>
2022-09-27 14:27:43 -0400

    EX05 Completed

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -3,9 +3,9 @@ __author__ = "730578568"
 
 
 def only_evens(list1: list[int]) -> list[int]:
-    "This function finds the even numbers of the list the user puts in."
-    i = 0
-    list2 = list()
+    """This function finds the even numbers of the list the user puts in."""
+    i: int = 0
+    list2: list[int] = list()
     while i < len(list1):
         if list1[i] % 2 == 0:
             list2.append(list1[i])
@@ -14,32 +14,36 @@ def only_evens(list1: list[int]) -> list[int]:
 
 
 def concat(list1: list[int], list2: list[int]) -> list[int]:
-    "This function adds all the elements of the first list with all the elements of the second list."
+    """This function adds all the elements of the first list with all the elements of the second list."""
+    i: int = 0
+    list3: list[int] = list()
+    while i < len(list1):
+        list3.append(list1[i])
+        i += 1
     i = 0
-    list3 = list1
     while i < len(list2):
-        list1.append(list2[i])
+        list3.append(list2[i])
         i += 1
     return list3
 
 
 def sub(a_list: list[int], start: int, end: int) -> list[int]:
-    "This function returns from start to end indexes, but not including end."
-    i = start 
-    b_list = list()
+    """This function returns from start to end indexes, but not including end."""
+    i: int = start 
+    b_list: list[int] = []
     if start < 0:
         i = 0
-    if start > len(a_list):
-        end = len(a_list) - 1
+    if start >= len(a_list):
+        return b_list
     if end > len(a_list):
-        end == len(a_list) - 1
+        end = len(a_list)
     if end == start:
         return a_list
     if end < 0 or end <= 0:
         return []
     if len(a_list) == 0:
         return []
-    while i < end-1:
+    while i < end:
         b_list.append(a_list[i])
         i += 1
     return b_list
