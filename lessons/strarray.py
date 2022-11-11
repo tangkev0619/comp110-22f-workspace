8e811c13e35c2c9ca2dd103a27cd85f373e1ecdd
Kevin Tang <tangkev@ad.unc.edu>
2022-11-10 21:28:07 -0500

    Done

---
+++ lessons/strarray.py
@@ -0,0 +1,35 @@
+"""Examples of "vectorized" operations via magic methods. """
+
+from __future__ import annotations
+from typing import Union
+
+
+class StrArray: 
+    items: list[str]
+
+    def __init__(self, items: list[str]) -> None:
+        self.items = items
+
+    def __repr__(self) -> str:
+        return f"StrArray({self.items})"
+    
+    def __add__(self, rhs: Union[str, StrArray]) -> StrArray:
+        result: StrArray = StrArray([])
+        if isinstance(rhs, str): 
+            for item in self.items: 
+                result.items.append(item + rhs)
+        # or item += rhs
+        #    result.items.append(item)
+        else: 
+            assert len(self.items) == len(rhs.items)
+            for i in range(len(self.items)):
+                result.items.append(self.items[i] + rhs.items[i])
+        return result
+
+
+a: StrArray = StrArray(["Armando", "Pete", "Leaky"])
+b: StrArray= StrArray(["Bacot", "Nance", "Black"])
+print(a)
+print(a + "!")
+print(a + b)
+print(b + "," + a + "!")
